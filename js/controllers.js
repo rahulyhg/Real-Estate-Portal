2dd77a49d777ff2205fb76138a222ed64824b1a6
vshetkar <vilass@cybage.com>
2015-01-28 12:34:33 +0530

    controller updated... with projectCtrl, propertyCtrl added.
    propertyCtrl added in previewprop.html.

--- js/controllers.js
+++ js/controllers.js
@@ -23,26 +23,40 @@ controller('responseCtrl', function($scope,$http, $routeParams, $location) {
 		.success(function(response) {$scope.resopnses = response;});
 	}
 }).
-controller('propertyCtrl', function($scope,$http, $routeParams, $location) {
-	if(!$routeParams.type && !$routeParams.status){
-		$location.path( "/property/web/all" );
-	}
-	$scope.type = $routeParams.type;
-	$scope.status = $routeParams.status;
-	$scope.id = $routeParams.id;
-	
-	console.log($scope.status);
-	$scope.getClass = function(path) {
-    if ($location.path().substr(0, path.length) == path) {return "active"} else {return ""}}
-	
-	//this request for all response data
-	$http.get("server-api/index.php/property")
-	.success(function(response) {$scope.resopnses = response;});
-	
+controller('projectCtrl', function($scope, $http, $routeParams) {
+
+	$scope.projects = { title: "vilas title"};
 	//this request for single response data
-	if($routeParams.id) {
-		$http.get("server-api/index.php/property/"+$routeParams.id)
-		.success(function(response) {$scope.resopnses = response;});
+	if($routeParams.type) {
+		$http.get("server-api/index.php/project/"+$routeParams.type)
+		.success(function(response) {$scope.projects = response;
+			console.log($scope.projects);
+		});
+		
+	}else{
+		//this request for all response data
+		$http.get("server-api/index.php/project")
+		.success(function(response) {$scope.projects = response;
+			console.log($scope.projects);
+		});
+		
+	}
+}).
+controller('propertyCtrl', function($scope, $http, $routeParams) {
+
+	//this request for single response data
+	if($routeParams.type) {
+		$http.get("server-api/index.php/property/"+$routeParams.type)
+		.success(function(response) {$scope.properties = response;
+			console.log($scope.properties);
+		});
+		
+	}else{
+		//this request for all response data
+		$http.get("server-api/index.php/property")
+		.success(function(response) {$scope.properties = response;
+			console.log($scope.properties);
+		});
 	}
 });
 
