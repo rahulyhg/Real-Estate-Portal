658c1fc0620e064546afc57dafe6d65ee4f84dcb
Pooja Nikam <pooja@wtouch.n>
2015-02-17 14:58:54 +0530

    create modules folders

--- app/modules/property/propertyController.js
+++ app/modules/property/propertyController.js
@@ -10,10 +10,12 @@ define(['app', 'css!modules/property/property'], function (app) {
 				//template: '<span class="close" ng-click="cancel()">X</span><img class="img-responsive" src="'+ url +'" />', /* inline template */
 				controller: 'propertyController', /* apply controller to modal template */
 				size: 'lg', /* bootstrap modal size - empty for default, lg for large, sm for small */
-				resolve: {
+				resolve: 
+				{
 					property : function (){
 						$http.get("../server-api/index.php/property/"+propId)
-						.success(function(response) {
+						.success(function(response) 
+						{
 							return $scope.property = response;
 							console.log($scope.property);
 						});	
@@ -26,75 +28,78 @@ define(['app', 'css!modules/property/property'], function (app) {
 			modalInstance.result.then(function (selectedItem) {
 				//$scope.selected = selectedItem;
 				$log.info("selected.")
-			}, function () {
+			},
+			function () {
 				$log.info('Modal dismissed at: ' + new Date());
 			});
-		};
-		
+		};		
 		
 		if($routeParams.id) {
 		$http.get("../server-api/index.php/property/"+$routeParams.id)
-		.success(function(response) {$scope.properties = response;
-			console.log($scope.properties);
-		});		
-		}else{	
-		$http.get("../server-api/index.php/property")
-		.success(function(response) {$scope.properties = response;
-			//console.log($scope.properties);
-		});
-		
-		// Add property
-		$scope.reset = function() {
-			$scope.property = {};
-		};
-		$scope.addprop = function(){
-			console.log($scope.property);
-			$http.post("../server-api/index.php/addproperty", $scope.property)
 			.success(function(response) {
-				alert(response);
-				$scope.reset();
-			});
-		};
-	}
-	if($routeParams.id){
-		//Update Property
-		$http.get("../server-api/index.php/property/"+$routeParams.id)
-		.success(function(response) {
-			$scope.property = response;
-			$scope.reset = function() {
-				$scope.property = angular.copy($scope.property);
-			};
-			$scope.reset();
-			console.log($scope.property);			
-		})		
-		$scope.update = function(){
-			$http.put("../server-api/index.php/editproperty/"+$routeParams.id,$scope.property)
-			.success(function(response) {
-				alert(response);
-			});
+				$scope.properties = response;
+				console.log($scope.properties);
+		    });		
 		}
-	}
-	$scope.view1 = function () {
-			var modalInstance = $modal.open({
-				templateUrl: '<span class="close" ng-click="cancel()">X</span> <a href="#/viewProperty">', /* for open template outside current template */
-				//template: '<span class="close" ng-click="cancel()">X</span><a class="responsive" href="#/viewProperty"></a>', /* inline template */
-				controller: 'propertyController', /* apply controller to modal template */
-				size: 'lg', /* bootstrap modal size - empty for default, lg for large, sm for small */
-				resolve: {
-					items: function () {
-					return $scope.items;
-					}
+		else{	
+				$http.get("../server-api/index.php/property")
+				.success(function(response) {
+					$scope.properties = response;
+					//console.log($scope.properties);
+				});
+		
+				// Add property
+				$scope.reset = function() {
+					$scope.property = {};
+				};
+				$scope.addprop = function(){
+					console.log($scope.property);
+					$http.post("../server-api/index.php/addproperty", $scope.property)
+					.success(function(response) {
+						alert(response);
+						$scope.reset();
+					});
+				};
+	        }
+			if($routeParams.id){
+				//Update Property
+				$http.get("../server-api/index.php/property/"+$routeParams.id)
+				.success(function(response) {
+						$scope.property = response;
+						$scope.reset = function() {
+							$scope.property = angular.copy($scope.property);
+						};
+						$scope.reset();
+						console.log($scope.property);			
+				})		
+				$scope.update = function(){
+					$http.put("../server-api/index.php/editproperty/"+$routeParams.id,$scope.property)
+					.success(function(response) {
+						alert(response);
+					});
 				}
-			});
-			modalInstance.result.then(function (selectedItem) {
-				//$scope.selected = selectedItem;
-				$log.info("selected.")
-			}, function () {
-				$log.info('Modal dismissed at: ' + new Date());
-			});
-		};
-		};	
-    
+			}
+			$scope.view1 = function () {
+					var modalInstance = $modal.open({
+						templateUrl: '<span class="close" ng-click="cancel()">X</span> <a href="#/viewProperty">', /* for open template outside current template */
+						//template: '<span class="close" ng-click="cancel()">X</span><a class="responsive" href="#/viewProperty"></a>', /* inline template */
+						controller: 'propertyController', /* apply controller to modal template */
+						size: 'lg', /* bootstrap modal size - empty for default, lg for large, sm for small */
+						resolve: {
+							items: function () {
+							return $scope.items;
+							}
+						}
+					});
+					modalInstance.result.then(function (selectedItem) {
+						//$scope.selected = selectedItem;
+						$log.info("selected.")
+					}, function () {
+						$log.info('Modal dismissed at: ' + new Date());
+					});
+				};
+				};	
+			
 	// Inject controller's dependencies
 	propertyController.$inject = injectParams;
 	// Register/apply controller dynamically
