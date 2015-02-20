148a97bdf502bb5f2c567122e0393cd0e1f02ccf
Pooja Nikam <pooja@wtouch.n>
2015-02-20 12:01:40 +0530

    remove edit btn  into edit profile

--- app/modules/websites/websitesController.js
+++ app/modules/websites/websitesController.js
@@ -1,20 +1,12 @@
-
-
 'use strict';
-
 define(['app'], function (app) {
     var injectParams = ['$scope', '$injector'];
-
     // This is controller for this view
-	var mywebsitesController = function ($scope, $injector) {
-		console.log("this is mywebsites ctrl");
-    };
-	
-    
+	var websitesController = function ($scope, $injector) {
+		console.log("this is websites controller");
+    };    
 	// Inject controller's dependencies
 	mywebsitesController.$inject = injectParams;
 	// Register/apply controller dynamically
-    app.register.controller('mywebsitesController', mywebsitesController);
-	
-	
+    app.register.controller('websitesController', websitesController);	
 });
