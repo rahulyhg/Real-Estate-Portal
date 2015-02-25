e7d69e3b7fba21799b414ab728ad99e085369dd7
Sunita Karle <sunita@wtouch.in>
2015-02-25 18:17:26 +0530

    "edit profile modified"

--- app/modules/users/register/editprofileController.js
+++ app/modules/users/register/editprofileController.js
@@ -4,6 +4,28 @@ define(['app', 'css!modules/users/register/edit'], function (app) {
     // This is controller for this view
 	var editprofileController = function ($scope, $injector,$routeParams,$http) {
 		console.log("this is edit controller");	
+		// for date picker{Sunita}
+		
+		$scope.today = function() 
+		{
+			$scope.dt = new Date();
+		};
+		$scope.today();
+		$scope.open = function($event)
+		{
+			$event.preventDefault();
+			$event.stopPropagation();
+			$scope.opened = true;
+		};
+		$scope.dateOptions = {
+			formatYear: 'yy',
+			startingDay: 1
+		};
+
+		$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+		$scope.format = $scope.formats[0];
+		
+		//*** End date Picker Code
 		
 		if($routeParams.id){
 		//update record
