64605baebb126332aa2a7f87c51bc2f77277641d
vshetkar <vilass@cybage.com>
2015-03-02 15:54:02 +0530

    dbHelper updated with limit clause added & insert, update query modified.
    also example created in website.php module.

--- Server-api/modules/website.php
+++ Server-api/modules/website.php
@@ -4,29 +4,36 @@
 	$reqMethod = $app->request->getMethod();
 	
 	if($reqMethod=="GET"){
-		echo $reqMethod;
 		if(isset($id)){
 			$where['id'] = $id;
 			$data = $db->select("2_real_property", $where);
 			echo json_encode($data);
 			
 		}else{
-			echo $pageNo;
-			echo $records;
-			$where=[];
-			$limit = $pageNo.",".$records;
+			$where=[]; // this will used for user specific data selection.
+			$limit['pageNo'] = $pageNo; // from which record to select
+			$limit['records'] = $records; // how many records to select
+			
+			// this is used to select data with LIMIT & where clause
 			$data = $db->select("2_real_property", $where, $limit);
+			
+			// this is used to count totalRecords with only where clause
+			$totalRecords['totalRecords'] = count($db->select("2_real_property", $where)['data']);		
+			
+			// $data is array & $totalRecords is also array. So for final output we just merge these two arrays into $data array
+			$data = array_merge($totalRecords,$data);
 			echo json_encode($data);
 		}
 	}
 	
 	if($reqMethod=="POST"){
-		echo $reqMethod;
-		echo $body;
+		$insert = $db->insert("2_real_property", $body);
+		echo json_encode($insert);
 	}
 	
 	if($reqMethod=="PUT" || $reqMethod=="DELETE"){
-		echo $reqMethod;
-		echo $body;
+		$where['id'] = $id; // need where clause to update/delete record
+		$update = $db->update("2_real_property", $body, $where);
+		echo json_encode($update);
 	}
  ?>
