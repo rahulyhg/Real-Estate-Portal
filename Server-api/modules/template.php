260a5d4051e71984b3ac4bcc63c6d4bde8d55b6a
Pooja Nikam <pooja@wtouch.n>
2015-03-02 19:03:48 +0530

    add table name into .php file

--- Server-api/modules/template.php
+++ Server-api/modules/template.php
@@ -1,3 +1,39 @@
 <?php
-
+	require_once 'db/dbHelper.php';
+	$db = new dbHelper();
+	$reqMethod = $app->request->getMethod();
+	
+	if($reqMethod=="GET"){
+		if(isset($id)){
+			$where['id'] = $id;
+			$data = $db->select("templates", $where);
+			echo json_encode($data);
+			
+		}else{
+			$where=[]; // this will used for user specific data selection.
+			$limit['pageNo'] = $pageNo; // from which record to select
+			$limit['records'] = $records; // how many records to select
+			
+			// this is used to select data with LIMIT & where clause
+			$data = $db->select("templates", $where, $limit);
+			
+			// this is used to count totalRecords with only where clause
+			$totalRecords['totalRecords'] = count($db->select("templates", $where)['data']);		
+			
+			// $data is array & $totalRecords is also array. So for final output we just merge these two arrays into $data array
+			$data = array_merge($totalRecords,$data);
+			echo json_encode($data);
+		}
+	}
+	
+	if($reqMethod=="POST"){
+		$insert = $db->insert("templates", $body);
+		echo json_encode($insert);
+	}
+	
+	if($reqMethod=="PUT" || $reqMethod=="DELETE"){
+		$where['id'] = $id; // need where clause to update/delete record
+		$update = $db->update("templates", $body, $where);
+		echo json_encode($update);
+	}
  ?>
