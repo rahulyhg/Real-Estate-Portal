328d6c7f786fa024c9c099dabd85b50b21eb66f7
Aarti Kulkarni <aarti@wtouch.in>
2015-03-04 17:19:13 +0530

    add code for display data into table

--- app/modules/websites/websitesController.js
+++ app/modules/websites/websitesController.js
@@ -1,8 +1,8 @@
 'use strict';
 define(['app'], function (app) {
-    var injectParams = ['$scope', '$injector','$routeParams','$location'];
+    var injectParams = ['$scope', '$injector','$routeParams','$location','$http'];
     // This is controller for this view
-	var websitesController = function ($scope, $injector,$routeParams,$location) {
+	var websitesController = function ($scope, $injector,$routeParams,$location,$http) {
 		console.log("this is websites controller");
 		
 		//Code For Pagination
@@ -26,6 +26,49 @@ define(['app'], function (app) {
 			});
 		};	
 		
+		//code for database operation
+		//code for view website data
+		if($routeParams.id) {
+		
+			$http.get("../server-api/index.php/getsingle/website"+$routeParams.id)
+			.success(function(response) {$scope.websites = response;
+				console.log($scope.websites);
+			});
+		
+		}else{
+		//this request for all response data
+			if($routeParams.pageNo){
+				$http.get("../server-api/index.php/getmultiple/website"+$routeParams.pageNo)
+				.success(function(response) 
+				{$scope.websites = response;
+					console.log($scope.websites);
+				});
+			}
+		}
+		//edit websites
+		if($routeParams.id){
+			$http.get("../server-api/index.php/getsingle/website"+$routeParams.id)
+			.success(function(response) {
+				$scope.websites = response;
+				$scope.reset = function() {
+					$scope.websites = angular.copy($scope.websites);
+				};
+				$scope.reset();
+				console.log($scope.websites);
+			
+		}).error(function(err){
+			console.log(err);
+		});
+		
+		$scope.update = function(){
+			$http.put("../server-api/index.php/put/website"+$routeParams.id,$scope.websites)
+			.success(function(response) {
+				alert(response);
+			});
+		};
+		}
+        //end code
+	
 		$scope.webPart=$routeParams.webPart;
 		console.log($scope.webPart);
 		if(!$routeParams.webPart){
