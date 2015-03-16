ef6ec2f48f6dda21c5b0aa1b6d1b06b45e2c97a9
Sunita Karle <sunita@wtouch.in>
2015-03-16 19:05:33 +0530

    "addproject.html" new fields are added

--- app/realestateportaldb.sql
+++ app/realestateportaldb.sql
@@ -82,3 +82,52 @@ dob DATETIME( 6 ) NOT NULL ,
 domain_name VARCHAR( 255 ) NOT NULL ,
 status ENUM(  'YES',  'NO' ) NOT NULL
 )
+
+'create table "project" '
+
+CREATE TABLE project(
+id INT PRIMARY KEY NOT NULL ,
+title VARCHAR( 256 ) NOT NULL ,
+featured ENUM(  '0',  '1' ) NOT NULL ,
+STATUS ENUM(  '0',  '1' ) NOT NULL ,
+user_id INT NOT NULL ,
+verified ENUM(  '0',  '1' ) NOT NULL ,
+category VARCHAR( 256 ) NOT NULL ,
+TYPE VARCHAR( 256 ) NOT NULL ,
+overview VARCHAR( 256 ) NOT NULL ,
+project_images LONGTEXT NOT NULL ,
+builder VARCHAR( 256 ) NOT NULL ,
+amenities LONGTEXT NOT NULL ,
+specifications LONGTEXT NOT NULL ,
+location LONGTEXT NOT NULL ,
+layout_map LONGTEXT NOT NULL ,
+floor_plan LONGTEXT NOT NULL ,
+project_gallery LONGTEXT NOT NULL ,
+contact_details LONGTEXT NOT NULL ,
+domain VARCHAR( 255 ) NOT NULL
+);
+
+' create table for "property" '
+
+CREATE TABLE property(
+id INT PRIMARY KEY NOT NULL ,
+title VARCHAR( 256 ) NOT NULL ,
+property_for VARCHAR( 256 ) NOT NULL ,
+category VARCHAR( 256 ) NOT NULL ,
+project_name VARCHAR( 256 ) NOT NULL ,
+property_description VARCHAR( 256 ) NOT NULL ,
+property_info VARCHAR( 256 ) NOT NULL ,
+price INTEGER( 20 ) NOT NULL ,
+property_images LONGTEXT NOT NULL ,
+property_location LONGTEXT NOT NULL ,
+contact_details LONGTEXT NOT NULL ,
+optional_property_details LONGTEXT NOT NULL ,
+landmarks VARCHAR( 256 ) NOT NULL ,
+amenities VARCHAR( 256 ) NOT NULL ,
+domain VARCHAR( 255 ) NOT NULL ,
+featured ENUM(  '0',  '1' ) NOT NULL ,
+status ENUM(  '0',  '1' ) NOT NULL ,
+user_id INT NOT NULL ,
+verified ENUM(  '0',  '1' ) NOT NULL ,
+seo_details LONGTEXT NOT NULL
+)
