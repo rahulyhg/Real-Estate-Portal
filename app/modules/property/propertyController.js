d98a3cd867ee4978c4f213ddbb5368aa817b9e52
Pooja Nikam <pooja@wtouch.n>
2015-03-18 17:06:39 +0530

    work on pagination

--- app/modules/property/propertyController.js
+++ app/modules/property/propertyController.js
@@ -1,11 +1,66 @@
 'use strict';
 define(['app'], function (app) {
-    var injectParams = ['$scope', '$injector','$routeParams','$http','$log', 'modalService', '$rootScope'];
+    var injectParams = ['$scope', '$injector','$routeParams','$http','$log', 'modalService', '$rootScope','dataService'];
     // This is controller for this view
-	var propertyController = function ($scope, $injector,$routeParams,$http, $log, modalService, $rootScope) {
+	var propertyController = function ($scope, $injector,$routeParams,$http, $log, modalService, $rootScope,dataService) {
 		$rootScope.metaTitle = "Real Estate Properties";
-		$scope.open = function (url, propId) {
-			$http.get("../server-api/index.php/property/"+propId)
+		
+		//Code For Pagination
+		$scope.maxSize = 5;
+		$scope.totalRecords = "";
+		$scope.currentPage = 1;
+		$scope.pageItems = 10;
+		$scope.numPages = "";		
+		$scope.alerts = [];
+		$scope.currentDate = dataService.currentDate;
+		console.log($scope.currentDate);
+		
+		 //for alert {Pooja}		 
+		if($scope.status=="warning"){     
+			 $scope.alerts.push({type: 'error', msg: "Error to load data"});
+			 $scope.closeAlert = function(index) {
+				$scope.alerts.splice(index, 1);
+			 };
+		}	 
+		//function for close alert
+		
+		$scope.closeAlert = function(index) {
+			$scope.alerts.splice(index, 1);
+		}; 		
+		
+		//code for pagination
+		$scope.pageChanged = function(page) {			
+			dataService.get("getmultiple/property/"+page+"/"+$scope.pageItems)
+			.then(function(response) {
+				$scope.properties = response.data;
+				//console.log(response.data);				
+			});			
+		};	//end pagination
+		
+		//search filter function
+		$scope.searchFilter = function(statusCol, searchProp) {
+			$scope.search = {search: true};
+			$scope.filterStatus= {};
+			$scope.propStatus = {};
+			(searchProp =="") ? delete $scope.propStatus[statusCol] : $scope.filterStatus[statusCol] = searchProp;
+			angular.extend($scope.propStatus, $scope.filterStatus);
+			angular.extend($scope.propStatus, $scope.search);			
+				dataService.get("getmultiple/property/1/"+$scope.pageItems, $scope.propStatus)
+				.then(function(response) {  //function for userlist response
+					if(response.status == 'success'){
+						$scope.properties = response.data;
+						$scope.totalRecords = response.totalRecords;
+					}else{
+						$scope.properties = {};
+						$scope.totalRecords = {};
+						$scope.alerts.push({type: response.status, msg: response.message});
+					}					
+				});
+		}		
+		
+		//view single property modal
+		 $scope.open = function (url, propId) {
+			dataService.get("/property/"+propId)
 			.success(function(response) {
 				var modalDefaults = {
 					templateUrl: url,	// apply template to modal
@@ -16,30 +71,33 @@ define(['app'], function (app) {
 				modalService.showModal(modalDefaults, modalOptions).then(function (result) {
 					console.log("modalOpened");
 				});
+			});			
+		};	
+		$scope.ok = function () {
+			$modalOptions.close('ok');
+		};	//end of modal function
+		
+		
+		var myProperty = function(){			
+			dataService.get("/getmultiple/property/"+$scope.myProperty+"/"+$scope.pageItems)
+			.then(function(response) {  //function for property list response
+				if(response.status == 'success'){
+					$scope.totalRecords = response.totalRecords;
+					$scope.properties = response.data;
+					console.log(response.data);
+				}else{
+					$scope.alerts.push({type: response.status, msg: response.message});
+				}
 			});
-			
 		};
 		
-		//Code For Pagination
-		$scope.maxSize = 5;
-		$scope.totalRecords = "";
-		$scope.currentPage = 1;
-		$scope.pageItems = 10;
-		$scope.numPages = "";		
-
-		$scope.pageChanged = function() {
-			//$log.log('Page changed to: ' + $scope.currentPage);
-			$http.get("../server-api/index.php/properties/"+$scope.currentPage+"/"+$scope.pageItems)
-			.success(function(response) {
-				$scope.properties = response.properties;
-				//$scope.totalRecords = response.totalRecords;
-				//console.log($scope.properties);
-			});
-		};		
 		
-		// Single Property view
+		
+		
+		
+		/* // Single Property view
 		if($routeParams.id) {
-			$http.get("../server-api/index.php/property/"+$routeParams.id)
+			dataService.get("/property/"+$routeParams.id)
 			.success(function(response) {
 				$scope.properties = response;
 				console.log($scope.properties);
@@ -47,13 +105,13 @@ define(['app'], function (app) {
 		}
 		// Multiple Property View
 		else{	
-			$http.get("../server-api/index.php/properties/"+$scope.currentPage+"/10")
+			dataService.get("/properties/"+$scope.currentPage+"/10")
 			.success(function(response) {
 				$scope.properties = response.properties;
 				$scope.totalRecords = response.totalRecords;
 				//console.log($scope.properties);
 			});
-		}
+		} */		
 	};		
 	// Inject controller's dependencies
 	propertyController.$inject = injectParams;
