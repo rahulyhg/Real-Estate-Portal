85cf203d49318ee32b84318fd35b72dcf1b82895
Pooja Nikam <pooja@wtouch.n>
2015-03-28 20:42:55 +0530

    add object from property field

--- app/modules/property/addproperty/addpropertyController.js
+++ app/modules/property/addproperty/addpropertyController.js
@@ -5,45 +5,27 @@ define(['app'], function (app) {
 	var addpropertyController = function ($scope, $injector,$routeParams,$http,$rootScope, upload, $timeout,dataService) {
 		$rootScope.metaTitle = "Add Real Estate Property";
 		
-		// Add property
-		$scope.reset = function() {
-			$scope.property = {};
-		};
-		$scope.addprop = function(){
-			console.log($scope.property);
-			dataService.post("post/property", $scope.property)
-			.then(function(response) {
-				alert(response);
-				$scope.reset();
-			});
-		};
-		var where = {name : "vilas"};
-		dataService.get("getsingle/property/2", where)
-			.then(function(response) {
-				//$scope.property = response.data;
-				console.log(response);
-			});
-			
-		/* if($routeParams.id){
-			//Update Property
-			dataService.get("getsingle/property/"+$routeParams.id, where)
-			.then(function(response) {
-					$scope.property = response;
-					$scope.reset = function() {
-						$scope.property = angular.copy($scope.property);
-					};
-					$scope.reset();
-					//console.log($scope.property);			
-			});	
-			$scope.update = function(){
-				$http.put("../server-api/index.php/editproperty/"+$routeParams.id,$scope.property)
-				.success(function(response) {
-					alert(response);
-				});
-			};
-		} */
+		$scope.alerts = [];
+		$scope.userinfo = {user_id : $rootScope.userDetails.id};
+		$scope.currentDate = dataService.currentDate;
 		
-		//Upload Function for uploading files {Vilas}
+		// Add property
+		$scope.addProperty = function(property){
+			console.log(property);
+			dataService.post("post/property", $scope.property,$scope.userinfo)
+			.then(function(response) {
+				if(response.status=="success"){
+					$scope.alerts.push({type: response.status, msg: response.message});
+				}else{
+					$scope.alerts.push({type: response.status, msg: response.message});
+				}
+				$scope.addproject.$setPristine();
+			});
+		};
+			
+		
+		
+		/* //Upload Function for uploading files {Vilas}
 		$scope.property={
 			prop_image : {},
 			domain : [{id:"2", domain_name : "www.wtouch.in"}]
@@ -67,7 +49,7 @@ define(['app'], function (app) {
 		$scope.generateThumb = function(files){  // this function will generate thumbnails of images
 			upload.generateThumbs(files);
 		};
-		// End upload function {Vilas}
+		// End upload function {Vilas} */
 		
 		dataService.get('getmultiple/website/1/200', {user_id:$rootScope.userDetails.id})
 		.then(function(response){
@@ -92,6 +74,9 @@ define(['app'], function (app) {
 			}
 		};
 	};		
+	
+	
+	
 	// Inject controller's dependencies
 	addpropertyController.$inject = injectParams;
 	// Register/apply controller dynamically
